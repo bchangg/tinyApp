bd94b20aa3d59a0ffa9d9e6d983d8c71f3c8603f
Brian Chang <changbhc@gmail.com>
2019-07-09 00:24:15 +0000

    Added template html into urls_index.ejs. Modified app.get(/urls) to take a template variable which contains the url database that we created as a constant earlier in the express_server.js file

--- express_server.js
+++ express_server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -17,12 +22,8 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
-  };
-
-  res.render("urls_index", urlDatabase);
+  let templateVars = { urls: urlDatabase }
+  res.render("urls_index", templateVars);
 })
 
 app.listen(PORT, () => {
