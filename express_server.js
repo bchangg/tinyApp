e3b405e0cee03fd68037b3a36efd232c2fb98096
Brian Chang <changbhc@gmail.com>
2019-07-09 08:19:52 +0000

    Added form POST method, added button. Delete funcitonality works as expected now.

--- express_server.js
+++ express_server.js
@@ -25,45 +25,50 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // NOTE: GET REQUESTS
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/", (req, resp) => {
+  resp.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls.json", (req, resp) => {
+  resp.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/hello", (req, resp) => {
+  resp.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls", (req, res) => {
+app.get("/urls", (req, resp) => {
   let templateVars = { urls: urlDatabase }
-  res.render("urls_index", templateVars);
+  resp.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get("/urls/new", (req, resp) => {
+  resp.render("urls_new");
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get("/urls/:shortURL", (req, resp) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+  resp.render("urls_show", templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
+app.get("/u/:shortURL", (req, resp) => {
   let destination = urlDatabase[req.params.shortURL];
-  res.redirect(`${destination}`);
+  resp.redirect(`${destination}`);
 });
 
 // NOTE: POST REQUESTS
-app.post("/urls", (req, res) => {
+app.post("/urls", (req, resp) => {
   const shortened = shortenedURL();
   if((req.body.longURL).substr(0, 7) !== 'http://') {
     req.body.longURL = 'http://' + req.body.longURL;
   }
   urlDatabase[shortened] = req.body.longURL;
-  res.redirect(`/urls/${shortened}`);
+  resp.redirect(`/urls/${shortened}`);
+});
+
+app.post("/urls/:shortURL/delete", (req, resp) => {
+  delete urlDatabase[req.params.shortURL];
+  resp.redirect("/urls");
 });
 
 // NOTE: SERVER 'START'
