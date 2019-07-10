c0c22cf8184f0d19cc22702c0c7e75abb0d8cb51
Brian Chang <changbhc@gmail.com>
2019-07-10 21:03:49 +0000

    Made a shitty template page for errors. Confirmed that the /register endpoint does the required functionality. Commiting before 'passing the user object into the _header'

--- database/helperFunctions.js
+++ database/helperFunctions.js
@@ -10,7 +10,7 @@ const random6CharString = function generateRandomString() {
   return result;
 }
 
-const emailExistsLookup = function findIfUserIsInDatabase(email, database) {
+const emailExists= function findIfUserIsInDatabase(email, database) {
   for(let user in database) {
     if(database[user].email === email) {
       return true;
@@ -20,7 +20,27 @@ const emailExistsLookup = function findIfUserIsInDatabase(email, database) {
 }
 
 
+// TESTING
+// const db = {
+//   user1: {
+//     id: "bob",
+//     email: "bob@bob.com",
+//     password: "12345"
+//   },
+//   user2: {
+//     id: "alice",
+//     email: "alice@alice.com",
+//     password: "12345"
+//   }
+// }
+// 
+// const db2 = {};
+// 
+// console.log(emailExists("bob@bob.com", db));
+// console.log(emailExists("alice@alice.com", db));
+// console.log(emailExists("bob@bob.com", db2));
+
 module.exports = {
   random6CharString,
-  emailExistsLookup
+  emailExists
 };
--- express_server.js
+++ express_server.js
@@ -1,22 +1,12 @@
 const { users } = require("./database/database");
 const { urlDatabase } = require("./database/database");
+const { random6CharString } = require("./database/helperFunctions");
+const { emailExists } = require("./database/helperFunctions");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080;
-
-// Sampled from:
-// https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-const shortenedURL = function generateRandomString() {
-  let result = "";
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const length = characters.length;
-  for (let i = 0; i < 6; i++) {
-    result += characters.charAt(Math.floor(Math.random() * length));
-  }
-  return result;
-}
 
 // NOTE: SERVER SETTINGS
 app.set("view engine", "ejs");
@@ -35,14 +25,14 @@ app.get("/urls.json", (request, response) => {
 app.get("/urls", (request, response) => {
   let templateVars = {
     urls: urlDatabase,
-    username: request.cookies["username"]
+    username: request.cookies["user_id"]
   }
   response.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (request, response) => {
   let templateVars = {
-    username: request.cookies["username"]
+    username: request.cookies["user_id"]
   }
   response.render("urls_new", templateVars);
 });
@@ -51,7 +41,7 @@ app.get("/urls/:shortURL", (request, response) => {
   let templateVars = {
     shortURL: request.params.shortURL,
     longURL: urlDatabase[request.params.shortURL],
-    username: request.cookies["username"]
+    username: request.cookies["user_id"]
   };
   response.render("urls_show", templateVars);
 });
@@ -62,7 +52,7 @@ app.get("/u/:shortURL", (request, response) => {
 
 app.get("/register", (request, response) => {
   let templateVars = {
-    username: request.cookies["username"]
+    username: request.cookies["user_id"]
   };
   response.render("register_user", templateVars);
 });
@@ -74,7 +64,7 @@ app.post("/urls", (request, response) => {
   } else if ((request.body.longURL).substr(0, 11) !== 'http://www.') {
     request.body.longURL = 'http://www.' + request.body.longURL;
   }
-  const shortened = shortenedURL();
+  const shortened = random6CharString();
   urlDatabase[shortened] = request.body.longURL;
   response.redirect(`/urls/${shortened}`);
 });
@@ -90,19 +80,29 @@ app.post("/urls/:shortURL/delete", (request, response) => {
 });
 
 app.post("/login", (request, response) => {
-  response.cookie("username", request.body.username);
+  response.cookie("user_id", request.body.username);
   response.redirect("/urls");
 });
 
 app.post("/logout", (request, response) => {
-  response.clearCookie("username", request.body.username);
+  response.clearCookie("user_id", request.body.username);
   response.redirect("/urls");
 });
 
-// COMBAK: FIX THIS POST FUNCTION SO THAT IT STORES STUFF INTO THE USERS "DATABASE" OBJECT
 app.post("/register", (request, response) => {
-  users[request.body.email] = {};
-  response.end("Success");
+  if (request.body.email === "" || request.body.password === "") {
+    response.status(400).render("error");
+  } else if (emailExists(request.body.email, users)) {
+    response.status(400).render("error");
+  } else {
+    const userRegisterRandomString = random6CharString();
+    users[userRegisterRandomString] = {};
+    users[userRegisterRandomString].id = userRegisterRandomString;
+    users[userRegisterRandomString].email = request.body.email;
+    users[userRegisterRandomString].password = request.body.password;
+    response.cookie("user_id", userRegisterRandomString);
+    response.redirect("/urls");
+  }
 });
 
 // NOTE: SERVER 'START'
